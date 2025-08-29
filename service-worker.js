218f483834659096cf6039390a9469fe214205ac
siewers32 <siewers32@users.noreply.github.com>
2025-08-29 19:41:27 +0000

    deploy: 47938322292c0b3bda2f6f25e84595e8c23909e1

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'mijn-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/script.js',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache geopend');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
